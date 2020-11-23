3ff5eeb158b1d129a8680629890cbf368c989d5e
UltraInstinct <55319714+UltraInstinct46@users.noreply.github.com>
2020-11-23 19:43:09 +0700

    migration

---
+++ database/migrations/2020_11_23_121806_create_transaksi_table.php
@@ -0,0 +1,37 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateTransaksiTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('transaksi', function (Blueprint $table) {
+            $table->id();
+            $table->string('kode_transaksi');
+            $table->bigInteger('obat_id');
+            $table->string('nama_kasir');
+            $table->integer('jumlah_beli');
+            $table->integer('total_harga');
+            $table->date('tanggal_beli');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('transaksi');
+    }
+}
