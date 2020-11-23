a0ba242031c68a534452c1f7ecde481274df0cc7
Muhammad Ridwan Pratama <56720407+ridwanz02@users.noreply.github.com>
2020-11-23 22:21:04 +0700

    Login multi user

--- resources/views/user/login.blade.php
+++ resources/views/user/login.blade.php
@@ -31,7 +31,7 @@
               <div class="card-header"><h4>Login</h4></div>
 
               <div class="card-body">
-                <form method="POST" action="#" class="needs-validation" novalidate="">
+                <form method="POST" action="{{ route('postlogin') }}" class="needs-validation" novalidate="">
                   @csrf
                   <div class="form-group">
                     <label for="email">Email</label>
