13984302b01876b8b8d372dba8699a75200b4e6d
Muhammad Ridwan Pratama <56720407+ridwanz02@users.noreply.github.com>
2020-11-23 22:27:08 +0700

    Add route midlleware

--- routes/web.php
+++ routes/web.php
@@ -19,46 +19,52 @@ Route::get('/', function () {
     return view('welcome');
 });
 
-Route::get('dashboard', function () {
-    return view('dashboard');
-})->name('dashboard');
-
 Route::get('login', function () {
     return view('user.login');
 })->name('login');
 
-Route::get('user', function () {
-    return view('user.index');
-})->name('user');
+Route::post('postlogin', 'LoginController@login')->name('postlogin');
+Route::get('logout', 'LoginController@logout')->name('logout');
 
-Route::get('create_user', function () {
-    return view('user.create');
-})->name('create_user');
+Route::group(['middleware' => ['auth']], function () {
+    Route::get('dashboard', 'DashboardController@index')->name('dashboard');
+});
 
-Route::get('edit_user', function () {
-    return view('user.edit');
-})->name('edit_user');
+Route::group(['middleware' => ['auth', 'ceklevel:admin']], function () {
+    Route::get('obat', function () {
+        return view('obat.index');
+    })->name('obat');
+    
+    Route::get('create_obat', function () {
+        return view('obat.create');
+    })->name('create_obat');
 
-Route::get('edit_obat', function () {
-    return view('obat.edit');
-})->name('edit_obat');
+    Route::get('edit_obat', function () {
+        return view('obat.edit');
+    })->name('edit_obat');
+    
+    Route::get('pemasok', function () {
+        return view('pemasok.index');
+    })->name('pemasok');
+    
+    Route::get('create_pemasok', function () {
+        return view('pemasok.create');
+    })->name('create_pemasok');
+    
+    Route::get('edit_pemasok', function () {
+        return view('pemasok.edit');
+    })->name('edit_pemasok');
+});
 
-Route::get('obat', function () {
-    return view('obat.index');
-})->name('obat');
+Route::group(['middleware' => ['auth', 'ceklevel:kasir']], function () {
+    
+});
 
-Route::get('create_obat', function () {
-    return view('obat.create');
-})->name('create_obat');
-
-Route::get('pemasok', function () {
-    return view('pemasok.index');
-})->name('pemasok');
-
-Route::get('create_pemasok', function () {
-    return view('pemasok.create');
-})->name('create_pemasok');
-
-Route::get('edit_pemasok', function () {
-    return view('pemasok.edit');
-})->name('edit_pemasok');
+Route::group(['middleware' => ['auth', 'ceklevel:manager']], function () {
+    Route::get('user', 'UserController@index')->name('user');
+    Route::get('create_user', 'UserController@create')->name('create_user');
+    Route::post('simpan_user', 'UserController@store')->name('simpan_user');
+    Route::get('edit_user/{id}', 'UserController@edit')->name('edit_user');
+    Route::post('update_user/{id}', 'UserController@update')->name('update_user');
+    Route::get('delete_user/{id}', 'UserController@destroy')->name('delete_user');
+});
