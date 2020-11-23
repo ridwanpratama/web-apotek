175f1f1c4179aa172ab0f9be5e60323a06017007
Muhammad Ridwan Pratama <56720407+ridwanz02@users.noreply.github.com>
2020-11-23 17:03:56 +0700

    Add files via upload

---
+++ resources/views/layouts/sidebar.blade.php
@@ -0,0 +1,65 @@
+<div class="main-sidebar">
+    <aside id="sidebar-wrapper">
+      <div class="sidebar-brand">
+        <a href="#">Apotek</a>
+      </div>
+      <div class="sidebar-brand sidebar-brand-sm">
+        <a href="#">Apotek</a>
+      </div>
+      <ul class="sidebar-menu">
+
+          <li class="menu-header">Dashboard</li>
+          <li class="nav-item dropdown">
+            <a href="#" class="nav-link ">
+              <i class="fas fa-fire"></i>
+              <span>Dashboard</span>
+            </a>
+          </li>
+
+            <li class="menu-header">Data Master</li>
+            <li class="nav-item dropdown">
+              <a href="" class="nav-link">
+                <i class="fas fa-columns"></i> 
+                <span>Data User</span>
+              </a>
+            </li>
+
+            <li class="nav-item dropdown">
+              <a class="nav-link" href="#">
+                <i class="far fa-square"></i> 
+                <span>Data Obat</span>
+              </a>
+            </li>
+          
+       
+          <li class="nav-item dropdown">
+            <a href="#" class="nav-link">
+              <i class="fas fa-ellipsis-h"></i> 
+              <span>Data Pemasok</span>
+            </a>
+          </li>
+   
+          <li class="nav-item dropdown">
+            <a href="#" class="nav-link">
+              <i class="fas fa-pencil-ruler"></i> 
+              <span>Transaksi</span>
+            </a>
+          </li>
+
+            <li class="nav-item dropdown">
+              <a href="#" class="nav-link">
+                <i class="far fa-file-alt"></i>
+                <span>Transaksi</span>
+              </a>
+            </li>
+
+            <li class="nav-item dropdown">
+              <a href="#" class="nav-link">
+                <i class="far fa-file-alt"></i>
+                <span>Laporan</span>
+              </a>
+            </li>
+
+        </ul>
+    </aside>
+  </div>
