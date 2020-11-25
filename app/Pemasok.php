c32246169555782553c5506c4bdbe09ee2004ea7
Muhammad Ridwan Pratama <56720407+ridwanz02@users.noreply.github.com>
2020-11-25 20:18:35 +0700

    Update relasi

--- app/Pemasok.php
+++ app/Pemasok.php
@@ -8,4 +8,9 @@ class Pemasok extends Model
 {
     protected $table = "pemasok";
     protected $fillable = ['kode_pemasok','nama_pemasok'];
+
+    public function pemasok()
+    {
+        return $this->hasMany(Obat::class);
+    }
 }
