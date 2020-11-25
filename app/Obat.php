d2925cb537b8718954c4ff2f2cc4260ac75a68bb
Muhammad Ridwan Pratama <56720407+ridwanz02@users.noreply.github.com>
2020-11-25 20:17:39 +0700

    Update relasi

--- app/Obat.php
+++ app/Obat.php
@@ -11,4 +11,14 @@ class Obat extends Model
     protected $table = "obat";
     protected $fillable = ['kode_obat','nama_obat','jenis_obat','harga_obat','stok_obat','pemasok_id'];
 
+    public function pemasok()
+    { 
+        return $this->belongsTo(Pemasok::class);
+    }
+
+    public function obat()
+    {
+        return $this->hasMany(Transaksi::class);
+    }
+
 }
