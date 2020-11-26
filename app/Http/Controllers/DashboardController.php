553023a8abc2cc1ac83b680920b497f53064db82
Muhammad Ridwan Pratama <56720407+ridwanz02@users.noreply.github.com>
2020-11-26 21:37:43 +0700

    Update DashboardController.php

--- app/Http/Controllers/DashboardController.php
+++ app/Http/Controllers/DashboardController.php
@@ -3,12 +3,20 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
+use App\User;
+use App\Pemasok;
+use App\Obat;
+use App\Transaksi;
 
 class DashboardController extends Controller
 {
     public function index()
     {
-        return view('dashboard');
+        $jumlah_user = User::all()->count();
+        $jumlah_obat = Obat::all()->count();
+        $jumlah_pemasok = Pemasok::all()->count();
+        $jumlah_transaksi = Transaksi::all()->count();
+        return view('dashboard', compact('jumlah_user', 'jumlah_obat', 'jumlah_pemasok', 'jumlah_transaksi'));
     }
 
 }
